430b768f8acc006701e68a401fdcb7b14fe09372
Alissa Mae Celeste <alissamaeceleste@gmail.com>
2024-01-06 17:14:34 +0800

    Made the footer absolute.

--- frontend/src/view/components/section/Footer.jsx
+++ frontend/src/view/components/section/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="flex bg-blue-600 justify-center">
+    <div className="flex bg-blue-600 justify-center w-full absolute bottom-0">
       <p className="text-white p-2">&#169; Copyright 2023 Lissa Ma Rie</p>
     </div>
   );
